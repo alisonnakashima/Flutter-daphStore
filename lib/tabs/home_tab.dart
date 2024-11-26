5815606dd3a2d3b7de755939b6a56dd0f8511e77
Alison Biazetto Nakashima <alisonbiazettonakashima@hotmail.com>
2024-11-26 02:04:17 -0300

    MainPage - OK (falta add mais imagens no firebase para o scrollficar melhor)

---
+++ lib/tabs/home_tab.dart
@@ -0,0 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter/src/painting/image_stream.dart';
+
+class HomeTab extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildBodyBack() => Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 89, 98),
+                Color.fromARGB(255, 253, 181, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+          )
+      ),
+    );
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack(),
+        CustomScrollView(
+          slivers: <Widget> [
+            const SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("Novidades"),
+                centerTitle: true,
+              ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: FirebaseFirestore.instance
+                  .collection("home").orderBy("pos").get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  return SliverMasonryGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    childCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var doc = snapshot.data!.docs[index];
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: (doc.data() as Map<String, dynamic>)["image"],
+                        fit: BoxFit.cover,
+                      );
+                    }
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
